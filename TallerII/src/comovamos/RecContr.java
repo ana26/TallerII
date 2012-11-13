802f6b31cc88935c3b4bff00b7168c572cc80630
Aldo Solano González <aldoglez34@gmail.com>
2012-11-13 16:26:44 -0600

    Ventana de recuperación de contraseña
    ,,

--- TallerII/src/comovamos/RecContr.java
+++ TallerII/src/comovamos/RecContr.java
@@ -35,8 +35,9 @@ public class RecContr extends javax.swing.JFrame {
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
-        jTextField4 = new javax.swing.JTextField();
+        contrasena = new javax.swing.JLabel();
+        pregunta = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
 
         jTextField1.setText("jTextField1");
 
@@ -62,43 +63,48 @@ public class RecContr extends javax.swing.JFrame {
         jLabel4.setFont(new java.awt.Font("Verdana", 0, 14)); // NOI18N
         jLabel4.setText("Tu contraseña es");
 
-        jLabel5.setFont(new java.awt.Font("Verdana", 0, 14)); // NOI18N
-        jLabel5.setText("contraseña");
+        contrasena.setFont(new java.awt.Font("Verdana", 1, 14)); // NOI18N
+        contrasena.setText(" ");
 
-        jTextField4.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField4ActionPerformed(evt);
-            }
-        });
+        pregunta.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        pregunta.setText(" ");
+
+        jLabel3.setFont(new java.awt.Font("Verdana", 0, 14)); // NOI18N
+        jLabel3.setText("Respuesta");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jTextField3, javax.swing.GroupLayout.DEFAULT_SIZE, 421, Short.MAX_VALUE)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(jLabel2)
-                                .addGap(18, 18, 18)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, 255, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(jTextField2, javax.swing.GroupLayout.DEFAULT_SIZE, 269, Short.MAX_VALUE)))))
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jLabel4)
-                        .addGap(18, 18, 18)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jButton2)
-                            .addComponent(jLabel5))))
-                .addContainerGap())
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(187, Short.MAX_VALUE)
-                .addComponent(jButton1)
-                .addGap(183, 183, 183))
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addGroup(layout.createSequentialGroup()
+                                    .addComponent(jLabel2)
+                                    .addGap(18, 18, 18)
+                                    .addComponent(jTextField2, javax.swing.GroupLayout.DEFAULT_SIZE, 270, Short.MAX_VALUE))
+                                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                    .addComponent(jLabel4)
+                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                    .addComponent(contrasena, javax.swing.GroupLayout.DEFAULT_SIZE, 290, Short.MAX_VALUE)))
+                            .addContainerGap())
+                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                            .addComponent(jButton1)
+                            .addGap(177, 177, 177))
+                        .addGroup(layout.createSequentialGroup()
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addGroup(layout.createSequentialGroup()
+                                    .addComponent(jLabel3)
+                                    .addGap(18, 18, 18)
+                                    .addComponent(jTextField3, javax.swing.GroupLayout.DEFAULT_SIZE, 330, Short.MAX_VALUE))
+                                .addComponent(pregunta, javax.swing.GroupLayout.DEFAULT_SIZE, 422, Short.MAX_VALUE))
+                            .addContainerGap()))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jButton2)
+                        .addGap(135, 135, 135))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -109,17 +115,19 @@ public class RecContr extends javax.swing.JFrame {
                     .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addComponent(jButton1)
-                .addGap(27, 27, 27)
-                .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(72, 72, 72)
+                .addComponent(pregunta)
                 .addGap(18, 18, 18)
-                .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3))
                 .addGap(18, 18, 18)
                 .addComponent(jButton2)
-                .addGap(58, 58, 58)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 89, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel4)
-                    .addComponent(jLabel5))
-                .addContainerGap(125, Short.MAX_VALUE))
+                    .addComponent(contrasena))
+                .addGap(51, 51, 51))
         );
 
         pack();
@@ -130,30 +138,48 @@ public class RecContr extends javax.swing.JFrame {
     ResultSet x=null;
     x=recuperarPass(jTextField2.getText());
     System.out.println(x);
-        try {
-            jTextField4.setText(String.valueOf(x.getString(1)));
+        try {x.next();
+            pregunta.setText((x.getString(1)));
         } catch (SQLException ex) {
             Logger.getLogger(RecContr.class.getName()).log(Level.SEVERE, null, ex);
         }
-
-
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        
+    ResultSet z=null;
+    z=recuperarContr(jTextField2.getText());
+     System.out.println(z);
+     try {z.next();
+            contrasena.setText((z.getString(1)));
+        } catch (SQLException ex) {
+            Logger.getLogger(RecContr.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }//GEN-LAST:event_jButton2ActionPerformed
 
-    private void jTextField4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField4ActionPerformed
-        
-    }//GEN-LAST:event_jTextField4ActionPerformed
+    
+    
+public ResultSet recuperarContr(String usuario) {
+        ResultSet contr = null;
+        try {
+          Class.forName("com.mysql.jdbc.Driver").newInstance();
+          con = new Conexion("tallerii1202","paul123");
+          contr = con.select(con, "com_usuarios.USU_respuesta", "com_usuarios", "WHERE com_usuarios.USU_usuario ='"+usuario+"'");
+    }
+        catch(Exception e) {
+            javax.swing.JOptionPane.showMessageDialog(null, e.toString());
+        }
+        return contr;
+    }
 
-    public ResultSet recuperarPass(String usuario) {
+public ResultSet recuperarPass(String usuario) {
             ResultSet pass = null;
         try {
             Class.forName("com.mysql.jdbc.Driver").newInstance();
-            Connection conn = DriverManager.getConnection("jdbc:mysql://148.226.81.254:3306/comovamos","tallerii1202","paul123");
-            Statement instruccion = conn.createStatement();
-            pass = instruccion.executeQuery("select com_preguntassecretas.PRE_Pregunta from com_preguntassecretas inner join com_usuarios on com_usuarios.USU_PreguntaSecreta = com_preguntassecretas.PRE_id where com_usuarios.USU_usuario = '"+usuario+"';");
+            con = new Conexion("tallerii1202","paul123");
+            //Statement instruccion = conn.createStatement();
+            //pass = instruccion.executeQuery("select com_preguntassecretas.PRE_Pregunta from com_preguntassecretas inner join com_usuarios on com_usuarios.USU_PreguntaSecreta = com_preguntassecretas.PRE_id where com_usuarios.USU_usuario = '"+usuario+"'");
+            //pass=con.select(con, "com_preguntassecretas.PRE_Pregunta", "com_preguntassecretas JOIN com_usuarios", "ON com_usuarios.USU_PreguntaSecreta = com_preguntassecretas.PRE_id WHERE com_usuarios.USU_usuario ='"+usuario+"'");
+            pass=con.select(con, "com_usuarios.USU_pregunta", "com_usuarios", "WHERE com_usuarios.USU_usuario ='"+usuario+"'");
         }
         catch(Exception e) {
             javax.swing.JOptionPane.showMessageDialog(null, e.toString());
@@ -170,15 +196,16 @@ public class RecContr extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel contrasena;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
     private javax.swing.JTextField jTextField3;
-    private javax.swing.JTextField jTextField4;
+    private javax.swing.JLabel pregunta;
     // End of variables declaration//GEN-END:variables
-
+    Conexion con;
 }
