52410f26fe5fa8424a3672321155ab89f069f320
lalo01 <a.066@hotmail.com>
2012-11-24 12:53:27 -0600

    Refactorización

--- TallerII/src/comovamos/Ventana_inicio.java
+++ TallerII/src/comovamos/Ventana_inicio.java
@@ -35,15 +35,15 @@ public class Ventana_inicio extends javax.swing.JFrame {
         buttonGroup4 = new javax.swing.ButtonGroup();
         jPopupMenu1 = new javax.swing.JPopupMenu();
         jFrame1 = new javax.swing.JFrame();
-        jPanel1 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
+        Panel_interno = new javax.swing.JPanel();
+        Label_como = new javax.swing.JLabel();
+        Label_nombre = new javax.swing.JLabel();
         txt_usuario = new javax.swing.JTextField();
-        jLabel3 = new javax.swing.JLabel();
+        Label_cont = new javax.swing.JLabel();
         aceptar = new javax.swing.JButton();
         jLabel4 = new javax.swing.JLabel();
         txt_contra = new javax.swing.JPasswordField();
-        jButton1 = new javax.swing.JButton();
+        Salir = new javax.swing.JButton();
 
         jTextField1.setText("jTextField1");
 
@@ -62,13 +62,13 @@ public class Ventana_inicio extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jPanel1.setPreferredSize(new java.awt.Dimension(685, 20));
+        Panel_interno.setPreferredSize(new java.awt.Dimension(685, 20));
 
-        jLabel1.setFont(new java.awt.Font("Verdana", 1, 24));
-        jLabel1.setText("¿Cómo vamos?");
+        Label_como.setFont(new java.awt.Font("Verdana", 1, 24)); // NOI18N
+        Label_como.setText("¿Cómo vamos?");
 
-        jLabel2.setFont(new java.awt.Font("Verdana", 0, 14));
-        jLabel2.setText("Nombre de usuario");
+        Label_nombre.setFont(new java.awt.Font("Verdana", 0, 14)); // NOI18N
+        Label_nombre.setText("Nombre de usuario");
 
         txt_usuario.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -76,8 +76,8 @@ public class Ventana_inicio extends javax.swing.JFrame {
             }
         });
 
-        jLabel3.setFont(new java.awt.Font("Verdana", 0, 14));
-        jLabel3.setText("Contraseña");
+        Label_cont.setFont(new java.awt.Font("Verdana", 0, 14));
+        Label_cont.setText("Contraseña");
 
         aceptar.setText("Entrar");
         aceptar.addActionListener(new java.awt.event.ActionListener() {
@@ -100,63 +100,63 @@ public class Ventana_inicio extends javax.swing.JFrame {
             }
         });
 
-        jButton1.setText("Salir");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        Salir.setText("Salir");
+        Salir.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                SalirActionPerformed(evt);
             }
         });
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
+        javax.swing.GroupLayout Panel_internoLayout = new javax.swing.GroupLayout(Panel_interno);
+        Panel_interno.setLayout(Panel_internoLayout);
+        Panel_internoLayout.setHorizontalGroup(
+            Panel_internoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(Panel_internoLayout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(Panel_internoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(Panel_internoLayout.createSequentialGroup()
                         .addGap(95, 95, 95)
-                        .addComponent(jLabel1)
+                        .addComponent(Label_como)
                         .addGap(105, 105, 105))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel2)
-                            .addComponent(jLabel3))
+                    .addGroup(Panel_internoLayout.createSequentialGroup()
+                        .addGroup(Panel_internoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(Label_nombre)
+                            .addComponent(Label_cont))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addGroup(Panel_internoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                             .addComponent(txt_contra, javax.swing.GroupLayout.DEFAULT_SIZE, 262, Short.MAX_VALUE)
                             .addComponent(txt_usuario, javax.swing.GroupLayout.DEFAULT_SIZE, 262, Short.MAX_VALUE))
                         .addContainerGap())
-                    .addGroup(jPanel1Layout.createSequentialGroup()
+                    .addGroup(Panel_internoLayout.createSequentialGroup()
                         .addGap(262, 262, 262)
                         .addComponent(jLabel4)
                         .addContainerGap())))
-            .addGroup(jPanel1Layout.createSequentialGroup()
+            .addGroup(Panel_internoLayout.createSequentialGroup()
                 .addGap(125, 125, 125)
                 .addComponent(aceptar)
                 .addGap(57, 57, 57)
-                .addComponent(jButton1)
+                .addComponent(Salir)
                 .addContainerGap(122, Short.MAX_VALUE))
         );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+        Panel_internoLayout.setVerticalGroup(
+            Panel_internoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, Panel_internoLayout.createSequentialGroup()
                 .addContainerGap(51, Short.MAX_VALUE)
-                .addComponent(jLabel1)
+                .addComponent(Label_como)
                 .addGap(83, 83, 83)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
+                .addGroup(Panel_internoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(Label_nombre)
                     .addComponent(txt_usuario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel3)
+                .addGroup(Panel_internoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(Label_cont)
                     .addComponent(txt_contra, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel4)
                 .addGap(66, 66, 66)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                .addGroup(Panel_internoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(aceptar)
-                    .addComponent(jButton1))
+                    .addComponent(Salir))
                 .addGap(45, 45, 45))
         );
 
@@ -166,14 +166,14 @@ public class Ventana_inicio extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 420, Short.MAX_VALUE)
+                .addComponent(Panel_interno, javax.swing.GroupLayout.DEFAULT_SIZE, 420, Short.MAX_VALUE)
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 378, Short.MAX_VALUE)
+                .addComponent(Panel_interno, javax.swing.GroupLayout.DEFAULT_SIZE, 378, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
@@ -188,12 +188,10 @@ public class Ventana_inicio extends javax.swing.JFrame {
         try {
             Class.forName("com.mysql.jdbc.Driver").newInstance();
             Connection conn = DriverManager.getConnection("jdbc:mysql://148.226.81.254:3306/comovamos","tallerii1202","paul123");
-
             Statement instruccion = conn.createStatement();
             ResultSet resultado = instruccion.executeQuery("select * from com_usuarios where USU_usuario = '"+usuario+"' and USU_contraseña = '"+contra+"';");
             if (resultado.first()){
                 if(resultado.getString("USU_usuario").equals(usuario) && resultado.getString("USU_contraseña").equals(contra)) {
-
                   return true;
                 }
                 return false;
@@ -205,19 +203,11 @@ public class Ventana_inicio extends javax.swing.JFrame {
             javax.swing.JOptionPane.showMessageDialog(null, e.toString());
             return false;
         }
-    }
-
-
-
-
-
-
-
-
+     }
     private void aceptarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_aceptarActionPerformed
-            ActionListener eAceptar = new ActionListener() {
-            @Override
-           public void actionPerformed(ActionEvent e) {
+            //ActionListener eAceptar = new ActionListener() {
+           // @Override
+           //public void actionPerformed(ActionEvent e) {
                 try{
                     if ( txt_contra.getText().length() > 0) {
                         if(validarUsuario(txt_usuario.getText(),txt_contra.getText())) {
@@ -236,15 +226,10 @@ public class Ventana_inicio extends javax.swing.JFrame {
                                  sal2.writeUTF(txt_contra.getText());
                                  sal2.close();
                             }
-                            catch(Exception ex){
-                                
-                            }
-                            
+                            catch(Exception ex){                                
+                            }                          
                               Menu_Principal f = new Menu_Principal();
-                              f.setVisible(true);
-
-
-             
+                              f.setVisible(true);            
                         }
                         else {
                             JOptionPane.showMessageDialog(null, "El usuario y/o contraseña son incorrectos", "Error", JOptionPane.ERROR_MESSAGE);
@@ -260,12 +245,10 @@ public class Ventana_inicio extends javax.swing.JFrame {
                 catch(Exception e1){
 
                 }
-            }
-        };
-        aceptar.addActionListener(eAceptar);
-
-
-
+       //     }
+        //};
+//}
+        //aceptar.addActionListener(eAceptar);
 }//GEN-LAST:event_aceptarActionPerformed
 
     private void txt_usuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txt_usuarioActionPerformed
@@ -280,9 +263,9 @@ public class Ventana_inicio extends javax.swing.JFrame {
         });
     }//GEN-LAST:event_jLabel4MouseClicked
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void SalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SalirActionPerformed
     System.exit(0);
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_SalirActionPerformed
 
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -293,19 +276,19 @@ public class Ventana_inicio extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel Label_como;
+    private javax.swing.JLabel Label_cont;
+    private javax.swing.JLabel Label_nombre;
+    private javax.swing.JPanel Panel_interno;
+    private javax.swing.JButton Salir;
     private javax.swing.JButton aceptar;
     private javax.swing.ButtonGroup buttonGroup1;
     private javax.swing.ButtonGroup buttonGroup2;
     private javax.swing.ButtonGroup buttonGroup3;
     private javax.swing.ButtonGroup buttonGroup4;
-    private javax.swing.JButton jButton1;
     private javax.swing.JCheckBox jCheckBox1;
     private javax.swing.JFrame jFrame1;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
-    private javax.swing.JPanel jPanel1;
     private javax.swing.JPopupMenu jPopupMenu1;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JPasswordField txt_contra;
