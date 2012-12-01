8b6de0e6e9843a39d604109f398cb93f290f9826
Yolanda <hard_nighters@hotmail.com>
2012-12-01 14:17:46 -0600

    Actualizando

--- TallerII/src/comovamos/frmDetaPregunta.java
+++ TallerII/src/comovamos/frmDetaPregunta.java
@@ -1,5 +1,7 @@
 package comovamos;
 import java.awt.*;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.*;
@@ -30,10 +32,29 @@ public class frmDetaPregunta extends javax.swing.JInternalFrame {
     boolean reg, autoreg,salidareg;
     String codmod;
     int MAXIMIZED_BOTH;
+    public String usuario;
+    public String contraseña;
     /** Creates new form frmDetaPregunta */
     public frmDetaPregunta() {
         initComponents();
         Cargar();
+        initComponents();
+         this.usuario="";
+        this.contraseña="";
+        try{
+            FileInputStream ob=new FileInputStream("ob.obj");
+            ObjectInputStream sal=new ObjectInputStream(ob);
+            this.usuario=(String)sal.readUTF();
+            sal.close();
+
+            FileInputStream ob2=new FileInputStream("ob2.obj");
+            ObjectInputStream sal2=new ObjectInputStream(ob2);
+            this.contraseña=(String)sal2.readUTF();
+            sal2.close();}
+        catch(Exception e){
+
+        }
+
     }
 void Cargar(){
         String Usuario ="ozkar";
